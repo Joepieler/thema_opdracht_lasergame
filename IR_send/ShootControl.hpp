96e15c4229206a3c566ee2382679be98782c63df
Joepieler <bryan.campagne@student.hu.nl>
2018-11-01 14:16:37 +0100

    Update ShootControl.hpp

--- IR_send/ShootControl.hpp
+++ IR_send/ShootControl.hpp
@@ -6,31 +6,18 @@
 #include "button_listener.hpp"
 #include "IRControl.hpp"
 
-class ShootControl : public ButtonListener, IRControl, rtos::task<>{
+class ShootControl : public PlayerData, Weapon, rtos::task<>{
 private:
     hwlib::target::pin_out laser = hwlib::target::pin_out(hwlib::target::pins::d4);
     rtos::clock receive_clock;
-    rtos::flag triggerFlag;
-    IRControl IR_control;
-public:
-    ShootControl(IRControl & IR_control):
-        task("ShootControl"),
-        receive_clock(this, 100 * rtos::us, "receive_clock"),
-        triggerFlag(this, "trigger"),
-        IR_control(IR_control)        
-        {}
-        
-    virtual void buttonDetected() override{
-        triggerFlag.set();
-    }
-        
-        
-    void shoot(const int playernumber, const int typewapen){
-        laser.set(1);
+    rtos::flag shoot_flag;
+    IRControl & IR_control;
+    
+    uint16_t encode(unsigned const int player_ID, unsigned const int wapen_ID){
         uint16_t data = 1;
-        uint8_t playerData = playernumber;
+        uint8_t playerData = player_ID;
         data += (playerData<<1);
-        uint8_t gameData = typewapen;
+        uint8_t gameData = player_ID;
         data += (gameData<<6);
         uint8_t controlBits = 0;
         controlBits += ( ((data & 2)>>1) ^ ((data & 64)>>6) );
@@ -38,30 +25,36 @@ public:
         controlBits += (( ((data & 8)>>3) ^ ((data & 256)>>8) )<<2);
         controlBits += (( ((data & 16)>>4) ^ ((data & 512)>>9) )<<3);
         controlBits += (( ((data & 32)>>5) ^ ((data & 1024)>>10) )<<4);
-        data += (controlBits<<11);
-        send(data);
-        laser.set(0);
-    }    
+        data += (controlBits << 11);
+        return data;
+    }
     
+     
+    
+public:
+    ShootControl(const char * name, IRControl& IR_control):
+        task(4, name),
+        receive_clock(this, 100 * rtos::us, "receive_clock"),
+        shoot_flag(this, "shoot_flag"),
+        IR_control(IR_control)
+        {}
+        
+    void shoot(){
+        shoot_flag.set();
+    }
 
-    
-    
     void main() override{
-        enum status {idle, transmit};
+        enum status {idle};
         status state = status::idle;
         for (;;){
             switch(state){
                 case status::idle:{
                     //wait for butten input
-                    wait(triggerFlag);
-                    state = status::transmit;
+                    wait(shoot_flag);
+                    laser.set(1);
+                    IR_control.setSendData(encode(PlayerData.playerID.get() ,PlayerData.wapenID.get()));
+                    laser.set(0);
                     break;
-                }
-                case status::transmit:{
-                    shoot(3,4);
-                    hwlib::wait_ms(500);
-                    state = status::idle;
-                   break;
                 }
                 
             }
