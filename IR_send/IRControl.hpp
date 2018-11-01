8424200a1f1c87a7929ab86a2612be82134fb66f
Joepieler <bryan.campagne@student.hu.nl>
2018-11-01 14:18:36 +0100

    Update IRControl.hpp

--- IR_send/IRControl.hpp
+++ IR_send/IRControl.hpp
@@ -4,17 +4,17 @@
 #include "hwlib.hpp"
 
 
-class IRControl {
+class IRControl : public rtos::task<>{
 private:
     hwlib::target::d2_36kHz IR_led = hwlib::target::d2_36kHz();
+    rtos::channel< uint16_t, 1024 > send_data_channel;
     const int zero_pulse = 1600;
 	const int one_pulse = 800;
     const int zero_pause = 800;
     const int one_pause = 1600;
     const int middel_pause = 3000;
     const int end_pause = 4000;
-public:
-    
+
     void send(const uint16_t & data){
         uint16_t data1 = data;
         uint16_t data2 = data;
@@ -44,6 +44,29 @@ public:
             hwlib::wait_us(zero_pulse);
             IR_led.set(0);
             hwlib::wait_us(zero_pause);
+        }
+    }
+public:
+    IRControl():
+    task("IRControl"),
+    send_data_channel( this, "send_data_channel" )
+    {}
+    
+    void setSendData( uint16_t data ) {
+		send_data_channel.write( data );
+	}
+
+    void main()override{
+        enum status {wait_for_data};
+        status state = status::wait_for_data;
+        for (;;){
+            switch(state){
+                case status::wait_for_data:{
+                    wait(send_data_channel);
+                    send(send_data_channel.read());
+                    break;
+                }
+            }
         }
     }
 };
