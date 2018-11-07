83f8f75ec5edfbd437ca1cdddd1d9760a51f1529
jasperdezeeuw <31954242+jasperdezeeuw@users.noreply.github.com>
2018-11-07 16:33:30 +0100

    Update main.cpp

--- Gameleader/main.cpp
+++ Gameleader/main.cpp
@@ -13,8 +13,8 @@ int main(void){
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(500);
     
-    auto scl = hwlib::target::pin_oc( hwlib::target::pins::scl );
-    auto sda = hwlib::target::pin_oc( hwlib::target::pins::sda );
+    auto scl = hwlib::target::pin_oc( hwlib::target::pins::scl1 );
+    auto sda = hwlib::target::pin_oc( hwlib::target::pins::sda1 );
     auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
     auto display = hwlib::glcd_oled( i2c_bus, 0x3c );
     
